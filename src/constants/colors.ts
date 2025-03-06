7685b8402f43a984b175a425cf5fb0c64e905ca3
k0miker <165369924+k0miker@users.noreply.github.com>
2025-03-06 10:07:00 +0100

    hex

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -1,17 +1,17 @@
 const Colors = {
   light: {
     text: "#52595e",
-    background: "white",
-    icon: "black",
+    background: "#ffffff",
+    icon: "#000000",
     // accent: "#00a9d7",
     // accent: "#d0bf00",
     // accent: "#ffc132",
     // accent: "#ff8c00",
   },
   dark: {
-    text: "white",
+    text: "#ffffff",
     background: "#1D2022",
-    icon: "white",
+    icon: "#ffffff",
     // accent: "#00a9d7", // blue
     // accent: "#d0bf00", // yellow
     // accent: "#ffc132", // orange
