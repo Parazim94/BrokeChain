8ee2e7bf701e0374b75cf1320092252bad1bd303
k0miker <165369924+k0miker@users.noreply.github.com>
2025-03-20 10:49:23 +0100

    Layout-Anpassung zu verbessern.

--- src/screens/PrivacyTermsScreen.tsx
+++ src/screens/PrivacyTermsScreen.tsx
@@ -44,6 +44,8 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         // backgroundColor is set dynamically
+        maxWidth:1024,
+        marginHorizontal:"auto",
     },
     content: {
         padding: 20,
